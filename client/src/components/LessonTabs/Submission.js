94d630ef86fe1a4c07c8a1cdbf4e514668b06bed
ndkhaivn <ndkhaivn@gmail.com>
2020-04-19 17:57:25 +0700

    Allow adding files to submission
    Avoid adding selected files

--- client/src/components/LessonTabs/Submission.js
+++ client/src/components/LessonTabs/Submission.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import { useDropzone } from 'react-dropzone';
 import { Button, Intent } from '@blueprintjs/core';
 
@@ -6,7 +6,7 @@ const thumbsContainer = {
   display: 'flex',
   flexDirection: 'row',
   flexWrap: 'wrap',
-  marginTop: 16
+  marginTop: 16,
 };
 
 const thumb = {
@@ -19,73 +19,74 @@ const thumb = {
   height: 100,
   padding: 4,
   boxSizing: 'border-box',
-  position: 'relative'
+  position: 'relative',
 };
 
 const thumbInner = {
   display: 'flex',
   minWidth: 0,
-  overflow: 'hidden'
+  overflow: 'hidden',
 };
 
 const img = {
   display: 'block',
   width: 'auto',
-  height: '100%'
+  height: '100%',
 };
 
 export default function Submission(props) {
   const [files, setFiles] = useState([]);
-  const {getRootProps, getInputProps} = useDropzone({
+  const { getRootProps, getInputProps } = useDropzone({
     accept: 'image/*',
-    onDrop: acceptedFiles => {
-      setFiles(acceptedFiles.map(file => Object.assign(file, {
-        preview: URL.createObjectURL(file)
-      })));
-    }
+    onDrop: (acceptedFiles) => {
+      setFiles(
+        files.concat(
+          acceptedFiles
+            .filter(file => // filter files that have been uploaded before
+              !files.map(file2 => file2.name).includes(file.name)
+            ) // then attach preview
+            .map((file) => 
+             Object.assign(file, {
+              preview: URL.createObjectURL(file),
+            })
+          )
+        )
+      );
+    },
   });
-  
-  const thumbs = files.map(file => (
+
+  const thumbs = files.map((file) => (
     <div style={thumb} key={file.name}>
-      <Button 
+      <Button
         className="corner-button"
         icon="cross"
         intent={Intent.DANGER}
         onClick={() => {
-          setFiles(files.filter(curFile => curFile.name !== file.name));
+          setFiles(files.filter((curFile) => curFile.name !== file.name));
         }}
       />
       <div style={thumbInner}>
-        <img
-          src={file.preview}
-          style={img}
-        />
+        <img src={file.preview} style={img} />
       </div>
     </div>
   ));
 
-  useEffect(() => () => {
-    // Make sure to revoke the data uris to avoid memory leaks
-    files.forEach(file => URL.revokeObjectURL(file.preview));
-  }, [files]);
+  useEffect(
+    () => () => {
+      // Make sure to revoke the data uris to avoid memory leaks
+      files.forEach((file) => URL.revokeObjectURL(file.preview));
+    },
+    [files]
+  );
 
   return (
     <section className="container text-center">
-      <div {...getRootProps({className: 'dropzone'})}>
+      <div {...getRootProps({ className: 'dropzone' })}>
         <input {...getInputProps()} />
-        <Button
-          outlined
-        >
-          Click here to select images...
-        </Button>
+        <Button outlined>Click here to select images...</Button>
       </div>
-      <aside style={thumbsContainer}>
-        {thumbs}
-      </aside>
-      <Button
-        disabled={files.length===0}
-        intent={Intent.PRIMARY}
-      >
+      <aside style={thumbsContainer}>{thumbs}</aside>
+      <Button disabled={files.length === 0} intent={Intent.PRIMARY}>
         Submit
       </Button>
     </section>
