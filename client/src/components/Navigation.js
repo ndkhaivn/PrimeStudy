96afe84a3743eec78f6b454ebd3b8c78365b9b54
ndkhaivn <ndkhaivn@gmail.com>
2020-04-19 10:27:39 +0700

    Button to switch language

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,17 +1,20 @@
-import React, { Component } from "react";
-import { useTranslation } from "react-i18next";
+import React, { Component } from 'react';
+import { useTranslation } from 'react-i18next';
 import {
   Navbar,
   Classes,
   NavbarGroup,
   NavbarDivider,
   NavbarHeading,
-  AnchorButton,
-} from "@blueprintjs/core";
-import { Link } from "react-router-dom";
+  Button,
+  Alignment,
+} from '@blueprintjs/core';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function Navigation() {
   const { t, i18n } = useTranslation();
+  const [lang, setLang] = useState("en");
 
   return (
     <Navbar className={Classes.DARK}>
@@ -19,11 +22,25 @@ export default function Navigation() {
         <NavbarHeading>Prime Study</NavbarHeading>
         <NavbarDivider />
         <Link to="/schedule">
-          <AnchorButton text={t("Schedule")} minimal rightIcon="calendar" />
+          <Button text={t('Schedule')} minimal rightIcon="calendar" />
         </Link>
         <Link to="/lesson">
-          <AnchorButton text={t("Lesson")} minimal rightIcon="presentation" />
+          <Button text={t('Lesson')} minimal rightIcon="presentation" />
         </Link>
+      </NavbarGroup>
+      <NavbarGroup align={Alignment.RIGHT}>
+        <div className='bp3-select bp3-minimal'>
+          <select 
+            value={lang} 
+            onChange={(event) => {
+              setLang(event.currentTarget.value);
+              i18n.changeLanguage(event.currentTarget.value);
+            }}
+          >
+            <option value="en" selected> en </option>
+            <option value="vi">vi</option>
+          </select>
+        </div>
       </NavbarGroup>
     </Navbar>
   );
