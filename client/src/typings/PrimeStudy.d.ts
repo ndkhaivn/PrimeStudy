d26b47fc352a7c28597ed20f61ac4d2d9dfd907d
ndkhaivn <38336025+ndkhaivn@users.noreply.github.com>
2020-04-25 14:50:15 +0700

    Update data schema

--- client/src/typings/PrimeStudy.d.ts
+++ client/src/typings/PrimeStudy.d.ts
@@ -1,35 +1,70 @@
 namespace PS
 {
-    type PersonID = string;
-    type ClassID = string;
-    type TimeTableID = string;
+    type UserID = number;
+    type ClassID = number;
+    type TimeTableID = number;
+    type StudentID = number;
+    type SubjectID = number;
+    type LessonID = number;
+    type SubmissionID = number;
 
-    interface Person
+    interface User
     {
-        id: PersonID;
-        name: string;
-        full_name: string;
-        classes: ClassID[];
+        id: UserID;
+        first_name: string;
+        last_name: string;
+        class_id: ClassID;
     }
 
     interface Student extends Person
     {
-
+        id: StudentID;
+        order_in_class: number;
+        created: Date;
+        class_id: ClassID;
+        user_id: UserID;
     }
 
     interface Teacher extends Person
     {
-    }
-
-    interface TimeTable
-    {
-        class_id: ClassID;
+        id: TeacherID;
+        user_id: UserID;
+        classes: ClassID[];
     }
 
     interface Class
     {
-        students: PersonID[];
-        teacher: Person;
-        time_table: TimeTableID;
+        title: string;
+        students: UserID[];
+        teachers: TeacherID[];
+    }
+    
+    interface Subject
+    {
+        id: SubjectID;
+        name: string;
+        color: string;
+    }
+    
+    interface Lesson
+    {
+        id: LessonID;
+        title: string;
+        youtube_src: string;
+        content: string;
+        requirements: string;
+        date: Date;
+        submission_due: Date;
+    }
+    
+    interface Submission
+    {
+        id: SubmissionID;
+        uploads: string[];
+        lesson_id: LessonID;
+        student_id: StudentID;
+        checked: boolean;
+        submitted: Date;
+        feedback: string[];
     }
 }
