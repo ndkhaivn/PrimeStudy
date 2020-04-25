0030e251ec53e110da28468af1b302ded502d94c
Remtori <lqv.remtori@gmail.com>
2020-04-25 12:54:40 +0700

    Client: Add basic Database type information

---
+++ client/src/typings/PrimeStudy.d.ts
@@ -0,0 +1,35 @@
+namespace PS
+{
+	type PersonID = string;
+	type ClassID = string;
+	type TimeTableID = string;
+
+    interface Person
+    {
+		id: PersonID;
+        name: string;
+		full_name: string;
+		classes: ClassID[];
+    }
+
+    interface Student extends Person
+    {
+
+    }
+
+    interface Teacher extends Person
+    {
+    }
+
+    interface TimeTable
+    {
+        class_id: ClassID;
+    }
+
+    interface Class
+    {
+        students: PersonID[];
+        teacher: Person;
+        time_table: TimeTableID;
+    }
+}
