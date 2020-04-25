e0488b9261f78d06c4767dee3783632f9fd674d1
Remtori <lqv.remtori@gmail.com>
2020-04-25 16:48:02 +0700

    Client: toDateHeader will fall back to English

--- client/src/utility.ts
+++ client/src/utility.ts
@@ -1,11 +1,13 @@
 // Immutable empty obj to use as default value in hooks
 export const emptyObj = Object.freeze({});
 
-export function toDateHeader(date: Date, lang: string) {
-    if (lang === 'en') {
-        return date.toDateString();
-    } else if (lang === 'vi') {
+export function toDateHeader(date: Date, lang: string): string
+{
+    if (lang === 'vi')
+    {
         const parseDay = ['Chủ nhật', 'Thứ hai', 'Thứ ba', 'Thứ tư', 'Thứ năm', 'Thứ sáu', 'Thứ bảy'];
         return `${parseDay[date.getDay()]} ngày ${date.getDate()} tháng ${date.getMonth()} năm ${date.getFullYear()}`;
     }
+
+    return date.toDateString();
 }
